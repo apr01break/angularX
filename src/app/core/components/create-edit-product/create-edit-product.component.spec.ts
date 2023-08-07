91d407f233093dfa15185e18ea58df224ea3eee4
apr01break <apr011097@gmail.com>
2023-08-06 21:08:37 -0500

    Segundo commit

--- src/app/core/components/create-edit-product/create-edit-product.component.spec.ts
+++ src/app/core/components/create-edit-product/create-edit-product.component.spec.ts
@@ -10,11 +10,21 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 export class ProductTestingService {
-  
+
   createProduct(product: Product) {
     return of({
-      id: 'PM22', name: 'Tarjeta XYS', description: 'XYS description test', logo: 'https://img.freepik.com/vector-gratis/ilustracion-vector-banner-sello-circulo_53876-27183.jpg', date_release: new Date('2023-10-08'), date_revision: new Date('2024-10-08') 
+      id: 'PM22', name: 'Tarjeta XYS', description: 'XYS description test', logo: 'https://img.freepik.com/vector-gratis/ilustracion-vector-banner-sello-circulo_53876-27183.jpg', date_release: new Date('2023-10-08'), date_revision: new Date('2024-10-08')
     });
+  }
+
+  updateProduct(product: Product) {
+    return of({
+      id: 'PM22', name: 'Tarjeta XYS', description: 'XYS description test', logo: 'https://img.freepik.com/vector-gratis/ilustracion-vector-banner-sello-circulo_53876-27183.jpg', date_release: new Date('2023-10-08'), date_revision: new Date('2024-10-08')
+    });
+  }
+
+  validateExistingId(id: string) {
+    return of(false);
   }
 }
 
@@ -45,10 +55,45 @@ describe('CreateEditProductComponent', () => {
 
   it('should create the product', () => {
     let product: Product = {
-      id: 'PM22', name: 'Tarjeta XYS', description: 'XYS description test', logo: 'https://img.freepik.com/vector-gratis/ilustracion-vector-banner-sello-circulo_53876-27183.jpg', date_release: new Date('2023-10-08'), date_revision: new Date('2024-10-08') 
+      id: 'PM22', name: 'Tarjeta XYS', description: 'XYS description test', logo: 'https://img.freepik.com/vector-gratis/ilustracion-vector-banner-sello-circulo_53876-27183.jpg', date_release: new Date('2023-10-08'), date_revision: new Date('2024-10-08')
     };
     component.productService.createProduct(product).subscribe(data => {
       expect(data).toEqual(product);
     })
   });
+
+  it('should update the product', () => {
+    let product: Product = {
+      id: 'PM22', name: 'Tarjeta XYS', description: 'XYS description test', logo: 'https://img.freepik.com/vector-gratis/ilustracion-vector-banner-sello-circulo_53876-27183.jpg', date_release: new Date('2023-10-08'), date_revision: new Date('2024-10-08')
+    };
+    component.productService.updateProduct(product).subscribe(data => {
+      expect(data).toEqual(product);
+    })
+  });
+
+  it('should validate existing id', () => {
+    let idExists = false;
+    let id = '21QWE';
+    component.productService.validateExistingId(id).subscribe(data => {
+      expect(data).toEqual(idExists);
+    })
+  });
+
+  it('should add a year to date', () => {
+    let date = '2022-09-21';
+    let newDate = new Date('2023-09-21 00:00:00 GMT-0500');
+    let resultDate = component.addOneYearToDate(date);
+    expect(resultDate).toEqual(newDate);
+  });
+
+  it('should reset the form', () => {
+    component.resetForm();
+    expect(component.form.get('id')?.value).toBeNull();
+    expect(component.form.get('name')?.value).toBeNull();
+    expect(component.form.get('description')?.value).toBeNull();
+    expect(component.form.get('logo')?.value).toBeNull();
+    expect(component.form.get('date_release')?.value).toBeNull();
+    expect(component.form.get('date_revision')?.value).toBeNull();
+  });
+
 });
